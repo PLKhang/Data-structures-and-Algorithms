f0f3bfbdcea1baac9e08a5cc11277f4654137217
Lê Phúc Khang <plkhang090403@gmail.com>
2023-02-21 10:38:55 +0700

    do nothing

--- ThiTracNghiemDevC/headerTest.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int add(int, int);
-int sub(int, int);
-long long mul(int, int);
-double dev(int, int);
--- ThiTracNghiemDevC/main.cpp
+++ ThiTracNghiemDevC/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "headerTest.h"
+// #include "headerTest.h"
 
 using namespace std;
 
