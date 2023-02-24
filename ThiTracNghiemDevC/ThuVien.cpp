08792583d3e7614d02d2f7425f54e28fe3afee44
Phuong Nguyen <hnguyenphuong09@gmail.com>
2023-02-25 01:02:23 +0700

    thu vien

---
+++ ThiTracNghiemDevC/ThuVien.cpp
@@ -0,0 +1,134 @@
+#include"ThuVien.h"
+
+void gotoxy(short x,short y)
+{
+        HANDLE hConsoleOutput;
+        COORD Cursor_an_Pos = { x,y};
+        hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleCursorPosition(hConsoleOutput , Cursor_an_Pos);
+} 
+void TextColor(int color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+void SetBGColor(WORD color)
+{
+	HANDLE hConsoleOutput;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+
+	WORD wAttributes = screen_buffer_info.wAttributes;
+	color &= 0x000f;
+	color <<= 4;
+	wAttributes &= 0xff0f;
+	wAttributes |= color;
+
+	SetConsoleTextAttribute(hConsoleOutput, wAttributes);
+}
+void ClearScreen()
+{
+	SetBGColor(0);
+	TextColor(7);
+	HANDLE                     hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD                      count;
+	DWORD                      cellCount;
+	COORD                      homeCoords = { 0, 0 };
+
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+	cellCount = csbi.dwSize.X * csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+		hStdOut,
+		(TCHAR)' ',
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+		hStdOut,
+		csbi.wAttributes,
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition(hStdOut, homeCoords);
+}
+int wherex()
+{
+	HANDLE hConsoleOutput;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+	return screen_buffer_info.dwCursorPosition.X;
+}
+int wherey(void)
+{
+	HANDLE hConsoleOutput;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+	return screen_buffer_info.dwCursorPosition.Y;
+}
+void Box(int x, int y,string nd) {
+	TextColor(14);
+	for (int ix = x; ix <= x + 40; ix++) {
+		gotoxy(ix, y);
+		cout << char(196);
+		gotoxy(ix, y + 2);
+		cout << char(196);
+	}
+	for (int iy = y + 1; iy <= y + 1; iy++) {
+		gotoxy(x, iy);
+		cout << char(179);
+		gotoxy(x + 40, iy);
+		cout << char(179);
+	}
+	gotoxy(x, y); cout << char(218);
+	gotoxy(x, y + 2); cout << char(192);
+	gotoxy(x + 40, y); cout << char(191);
+	gotoxy(x + 40, y + 2); cout << char(217);
+	gotoxy(x +1, y+1 );
+	TextColor(1);
+	SetBGColor(7);
+	cout << "                                      ";
+	gotoxy(x + 1, y + 1); cout << nd;
+	gotoxy(x, y);
+}
+void ToMau(int x,int y,int mau,string nd) {
+	gotoxy(x + 1, y + 1);
+	TextColor(1);
+	SetBGColor(mau);
+	cout << "                                      ";
+	gotoxy(x + 1, y + 1); cout << nd;
+	gotoxy(x, y);
+}
+int NhapSo(int x, int y)// x va y la dia chi de hien ki tu vua nhap
+{
+    char so=getch();
+	if(so>='0'&&so<='1')return so-'0';
+	else return -1;
+}
+string NhapChuoi(int x,int y)// x va y la dia chi de hien ki tu vua nhap
+{
+	string chuoi;
+	int index;
+	char ki_tu=getch();
+	while(true)
+	{
+	 if((ki_tu>='A'&&ki_tu<='Z')||(ki_tu>='a'&&ki_tu<='z')||ki_tu=='_')
+	 chuoi[index++]=ki_tu;
+	 else if(ki_tu==13)break;
+	}
+	return chuoi;
+}
