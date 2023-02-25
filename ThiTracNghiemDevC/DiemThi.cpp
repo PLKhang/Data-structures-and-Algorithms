88dfd53fe60022f263039b2fa66d76d9844aee01
Lê Phúc Khang <plkhang090403@gmail.com>
2023-02-25 16:34:52 +0700

    update DiemThi.cpp

--- ThiTracNghiemDevC/DiemThi.cpp
+++ ThiTracNghiemDevC/DiemThi.cpp
@@ -5,7 +5,7 @@ void insert_First_DT(PtrDT &first, DiemThi x)
     if (is_Empty_DT(first))
         return;
 
-    PtrDT p = new DanhSachDiemThi;
+    PtrDT p = new nodeDiemThi;
 
     p->info = x;
     p->next = first;
@@ -16,7 +16,7 @@ void insert_After_DT(PtrDT p, DiemThi x)
 {
     if (p == NULL)
         return;
-    PtrDT q = new DanhSachDiemThi;
+    PtrDT q = new nodeDiemThi;
     q->info = x;
     q->next = p->next;
     p->next = q;
@@ -40,25 +40,6 @@ void detele_After_DT(PtrDT p)
     q = p->next;
     p->next = q->next;
     delete q;
-}
-
-int search_info_DT(PtrDT first, DiemThi x)
-{
-    if (is_Empty_DT(first))
-        return 0;
-
-    int count = 0;
-    if (first->info == x)
-        count++;
-
-    PtrDT p;
-    for (p = first; p != NULL && p->next != NULL; p = p->next)
-    {
-        if (p->next->info == x)
-            count++;
-    }
-
-    return count;
 }
 
 void clear_List_DT(PtrDT &first){
