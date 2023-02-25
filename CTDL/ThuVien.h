0387074d775776ffec54536789b5de66b565d862
Phuong Nguyen <hnguyenphuong09@gmail.com>
2023-02-25 19:11:48 +0700

    Cap nhat thu vien

--- CTDL/ThuVien.h
+++ CTDL/ThuVien.h
@@ -1,1 +1,21 @@
-
+#pragma once
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+//#include"ThuVien.cpp"
+#include<string>
+using namespace std;
+void gotoxy(short x,short y);
+void TextColor(int color) ;
+void SetBGColor(WORD color);
+void ClearScreen();
+int wherex(void);
+int wherey(void);
+void Box(int x, int y,string nd);
+void ToMau(int x,int y,int mau,string nd);
+char GetKey();
+int NhapSo(int x,int y);
+string NhapChuoi(int x,int y);
+void NhapMa();
+void VeKhung();
+void ThongBao();
