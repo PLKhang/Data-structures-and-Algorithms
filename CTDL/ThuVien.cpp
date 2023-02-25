8911fa991de404ea961c4c919e2b5145c5352fd2
Phuong Nguyen <hnguyenphuong09@gmail.com>
2023-02-26 01:11:44 +0700

    cap nhat thu vien

--- CTDL/ThuVien.cpp
+++ CTDL/ThuVien.cpp
@@ -1,11 +1,10 @@
 #include"ThuVien.h"
-void gotoxy(short x,short y)
-{
-        HANDLE hConsoleOutput;
-        COORD Cursor_an_Pos = { x,y};
-        hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleCursorPosition(hConsoleOutput , Cursor_an_Pos);
-} 
+void gotoxy(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 void TextColor(int color) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
@@ -25,97 +24,71 @@ void SetBGColor(WORD color)
 
 	SetConsoleTextAttribute(hConsoleOutput, wAttributes);
 }
-void ClearScreen()
-{
-	SetBGColor(0);
-	TextColor(7);
-	HANDLE                     hStdOut;
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	DWORD                      count;
-	DWORD                      cellCount;
-	COORD                      homeCoords = { 0, 0 };
-
-	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (hStdOut == INVALID_HANDLE_VALUE) return;
-
-	/* Get the number of cells in the current buffer */
-	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
-	cellCount = csbi.dwSize.X * csbi.dwSize.Y;
-
-	/* Fill the entire buffer with spaces */
-	if (!FillConsoleOutputCharacter(
-		hStdOut,
-		(TCHAR)' ',
-		cellCount,
-		homeCoords,
-		&count
-	)) return;
-
-	/* Fill the entire buffer with the current colors and attributes */
-	if (!FillConsoleOutputAttribute(
-		hStdOut,
-		csbi.wAttributes,
-		cellCount,
-		homeCoords,
-		&count
-	)) return;
-
-	/* Move the cursor home */
-	SetConsoleCursorPosition(hStdOut, homeCoords);
-}
-int wherex()
-{
-	HANDLE hConsoleOutput;
-	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
-	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
-	return screen_buffer_info.dwCursorPosition.X;
-}
-int wherey(void)
-{
-	HANDLE hConsoleOutput;
-	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
-	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
-	return screen_buffer_info.dwCursorPosition.Y;
-}
-void Box(int x, int y,string nd) {
-	TextColor(14);
-	for (int ix = x; ix <= x + 40; ix++) {
-		gotoxy(ix, y);
+void VeKhung(int x1, int y1,int x2,int y2,int mau_nen,int loai_khung) {
+	for (int ix = x1; ix <= x2; ix++) {
+		gotoxy(ix, y1);
 		cout << char(196);
-		gotoxy(ix, y + 2);
+		gotoxy(ix, y2 );
 		cout << char(196);
+		if(loai_khung==1)
+		{
+		gotoxy(ix, y1+1);
+		cout << char(196);
+		gotoxy(ix, y2-1 );
+		cout << char(196);	
+		}
 	}
-	for (int iy = y + 1; iy <= y + 1; iy++) {
-		gotoxy(x, iy);
+	for (int iy = y1+1; iy < y2; iy++) {
+		gotoxy(x1, iy);
 		cout << char(179);
-		gotoxy(x + 40, iy);
+		gotoxy(x2, iy);
 		cout << char(179);
+		if(loai_khung==1)
+		{
+		gotoxy(x1+1, iy);
+		cout << char(179);
+		gotoxy(x2-1, iy);
+		cout << char(179);
+		}
 	}
-	gotoxy(x, y); cout << char(218);
-	gotoxy(x, y + 2); cout << char(192);
-	gotoxy(x + 40, y); cout << char(191);
-	gotoxy(x + 40, y + 2); cout << char(217);
-	gotoxy(x +1, y+1 );
-	TextColor(1);
-	SetBGColor(7);
-	cout << "                                      ";
-	gotoxy(x + 1, y + 1); cout << nd;
-	gotoxy(x, y);
-}
-void ToMau(int x,int y,int mau,string nd) {
-	gotoxy(x + 1, y + 1);
-	TextColor(1);
-	SetBGColor(mau);
-	cout << "                                      ";
-	gotoxy(x + 1, y + 1); cout << nd;
-	gotoxy(x, y);
+	//Ve 4 goc vuong
+	gotoxy(x1, y1); cout << char(218);
+	gotoxy(x1, y2 ); cout << char(192);
+	gotoxy(x2 , y1); cout << char(191);
+	gotoxy(x2 , y2); cout << char(217);
+	if(loai_khung==1)
+	{
+	gotoxy(x1+1, y1+1); cout << char(218);
+	gotoxy(x1+1, y2-1 ); cout << char(192);
+	gotoxy(x2-1 , y1+1); cout << char(191);
+	gotoxy(x2-1 , y2-1); cout << char(217);
+	}
+	if(mau_nen!=-1)
+	{
+		int kt=1;
+		if(loai_khung==1)kt=2;
+		for (int ix = x1+kt; ix <= x2-kt; ix++) 
+		{
+			for (int iy = y1+kt; iy <= y2-kt; iy++)
+			{
+				gotoxy(ix,iy);
+				SetBGColor(mau_nen);
+				cout<<" ";
+			}
+		}
+		SetBGColor(0);
+	}
+
 }
 int NhapSo(int x, int y)// x va y la dia chi de hien ki tu vua nhap
 {
     char so=getch();
-	if(so>='0'&&so<='1')return so-'0';
+	if(so>='0'&&so<='9')
+	{
+		gotoxy(x,y);
+		cout<<so-'0';
+		return so-'0';
+	}
 	else return -1;
 }
 string NhapChuoi(int x,int y)// x va y la dia chi de hien ki tu vua nhap
@@ -125,7 +98,7 @@ string NhapChuoi(int x,int y)// x va y la dia chi de hien ki tu vua nhap
 	char ki_tu;
 	while((ki_tu=getch())!=13&&index<51)
 	{
-	 if((ki_tu>='A'&&ki_tu<='Z')||(ki_tu>='a'&&ki_tu<='z')||ki_tu=='_'||(ki_tu==32&&index!=0))
+	 if((ki_tu>='A'&&ki_tu<='Z')||(ki_tu>='a'&&ki_tu<='z')||(ki_tu==' '&&index!=0))
 	 {
 	 	if(ki_tu==32&&index>0&&chuoi[index-1]==32)continue;
 		chuoi[index]=ki_tu;
@@ -136,6 +109,7 @@ string NhapChuoi(int x,int y)// x va y la dia chi de hien ki tu vua nhap
 	 if(ki_tu==8&&index>0)
 	 {
 	 	index--;
+		chuoi[index]='\0';
 	 	gotoxy(x+index,y);
 	    cout<<" ";	
 	    gotoxy(x+index,y);
@@ -143,4 +117,69 @@ string NhapChuoi(int x,int y)// x va y la dia chi de hien ki tu vua nhap
 	}
 	chuoi[index]='\0';
 	return chuoi;
+}
+string NhapMa(int x,int y)
+{
+	char Ma[51],ki_tu;
+	int index=0;
+	while((ki_tu=getch())!=13&&index<51)
+	{
+		if(index==0&&((ki_tu>='0'&&ki_tu<='9')||ki_tu==' '))continue;
+		if((ki_tu>='A'&&ki_tu<='Z')||(ki_tu>='a'&&ki_tu<='z')||(ki_tu>='0'&&ki_tu<='9')||ki_tu=='_')
+		{
+			Ma[index]=ki_tu;
+			gotoxy(x+index,y);
+			cout<<ki_tu;
+			index++;
+		}
+		if(ki_tu==8&&index>0)
+	    {
+	 	index--;
+		Ma[index]='\0';
+	 	gotoxy(x+index,y);
+	    cout<<" ";	
+	    gotoxy(x+index,y);
+	   }
+	}
+	Ma[index]='\0';
+	return Ma;
+}
+bool ThongBao1(int x,int y,string noidung,int thong_bao)
+{
+	char ki_tu,vi_tri='t';
+	VeKhung(x,y,x+50,y+4,3,0);
+	cout<<noidung;
+	if(thong_bao==0)
+	{
+		return false;
+	}
+	    gotoxy(x+20,y+3);
+	    SetBGColor(5);
+	    cout<<"YES ";
+	    SetBGColor(1);
+	    cout<<"| NO";
+	while((ki_tu=getch())!=13)
+	{
+		if(ki_tu==75)
+		{
+		gotoxy(x+20,y+3);
+	    SetBGColor(5);
+	    cout<<"YES ";
+	    SetBGColor(1);
+	    cout<<"| NO";
+		vi_tri='t';
+		}
+		if(ki_tu==77)
+		{
+		gotoxy(x+20,y+3);
+	    SetBGColor(1);
+	    cout<<"YES ";
+	    SetBGColor(5);
+	    cout<<"| NO";
+	    vi_tri='p';
+		}
+	}
+	SetBGColor(0);
+	if(vi_tri=='t')return true;
+	else return false;
 }
